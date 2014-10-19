445440931d810ed749bca61dd1478bc5aa2ea842
Hoshiyo <guyen@et.esiea.fr>
2014-10-19 07:48:56 +0200

    first commit

---
+++ app/src/main/java/com/example/hoshiyo/wictrip/MapsActivity.java
@@ -0,0 +1,146 @@
+package com.example.hoshiyo.wictrip;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements LocationListener {
+
+    private static final long TIME_BETWEEN_TWO_POINTS = 50000;
+    private static final float DISTANCE_BETWEEN_TWO_POINTS = 3000;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private LocationManager locationManager;
+    private static String PROX_ALERT = "String fr.esiea.mobile.prox";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); //access different radar
+        IntentFilter filter = new IntentFilter(PROX_ALERT);
+        registerReceiver(new MyLocationPromixityReceiver(this), filter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+
+
+        //Listen location's change with criteria and say it to manager
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        String theBestProvider = locationManager.getBestProvider(criteria, true);
+        locationManager.requestLocationUpdates(
+                theBestProvider,
+                TIME_BETWEEN_TWO_POINTS,
+                DISTANCE_BETWEEN_TWO_POINTS,
+                this
+        );
+    }
+
+    //Stop listener when we are on pause
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
+
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+
+            @Override
+            public void onMyLocationChange(Location location) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude())));
+            }
+        });
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getBaseContext(), "Location changed", Toast.LENGTH_SHORT).show();
+        if(mMap != null) {
+            LatLng myLatLng = new LatLng(location.getLatitude(),location.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLatLng));
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Toast.makeText(getBaseContext(), "Status changed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Toast.makeText(getBaseContext(), "Provider enabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Toast.makeText(getBaseContext(), "Provider disabled", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setUpProximity() {
+        double latLocation = 48.81454;
+        double lngLocation = 2.37888;
+        float radius = 100;
+        long expiration = -1; //never
+        Intent intent = new Intent(PROX_ALERT); //intention a lever en cas d'entrée/sortie
+        PendingIntent proxIntent = PendingIntent.getBroadcast(this, -1, intent, 0);
+
+        locationManager.addProximityAlert(latLocation, lngLocation, radius, expiration, proxIntent); //add a position to listen
+    }
+}
